24b5af58b11e6f3bc52095c85cfe3a1d7b5a7453
Martin <manhdaokhacmanh@gmail.com>
2020-08-16 21:56:13 +0700

    update commit

--- main.py
+++ main.py
@@ -8,27 +8,33 @@ for row in csv.reader(open("googleplaystore.csv"), delimiter=','):
 # print(data)
 data = data[1:]
 remove_underline = lambda row: row[1].replace("_", " ")
-remove_sizeM = lambda row: row[4].replace("M", "000000")
+
+remove_sizeMk = lambda row: float(row[4].replace("M", "")) * 1000000 if row[4].find("M") != -1 \
+    else float(row[4].replace("k", "")) * 1000
+
 remove_nan_rating = lambda row: row[2].replace("NaN", "0")
 remove_plus = lambda row: row[5].replace("+", "")
-remove_various_device4 = lambda row: row.replace("Varies with device", "0")
-print(data[1])
-# remove_various_device11 = lambda row:
-row4v = list(map(remove_various_device4, data))
-# row11v = list(map(remove_various_device11, data))
+remove_vdevice4 = lambda row: 0 if row[4] == "Varies with device" else (remove_sizeMk)(row)
+convert_num11 = lambda row: 1.0 if row[11] == "Varies with device" \
+    else (1.0 if row[11] == "NaN" else row[11][:3])
+convert_num12 = lambda row: 1.0 if row[12] == "Varies with device" \
+    else (1.0 if row[12] == "NaN" else row[12][:3])
+
 # Clear underline in Category
 row1 = list(map(remove_underline, data))
-row4 = list(map(remove_sizeM, data))
+row4 = list(map(remove_vdevice4, data))
 row2 = list(map(remove_nan_rating, data))
+row5 = list(map(remove_plus, data))
+row11 = list(map(convert_num11, data))
+row12 = list(map(convert_num12, data))
 i = 0
 for row in data:
     row[1] = row1[i]
-    row[4] = row4v[i]
+    row[4] = float(row4[i])
     row[2] = float(row2[i])
-    # row[11] = row11v[i]
+    row[5] = float(row5[i].replace(",", ""))
+    row[11] = row11[i]
+    row[12] = row12[i]
     i = i + 1
 
-x = 0
-for row in data:
-    row[4] = float(row4[x])
-print(data[37])
+print(data)
